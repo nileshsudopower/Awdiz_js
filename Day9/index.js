7e0806a32baaaa5591ef95130a5cd51473794711
nileshsudopower <164172569+nileshsudopower@users.noreply.github.com>
2024-05-11 10:53:40 +0530

    Day 9 updates

---
+++ Day9/index.js
@@ -0,0 +1,27 @@
+// var array = [2,5,9,12,18,21]
+// var count = 0
+// for(i = 0 ; i < array.length ; i++){
+//     for(j = i + 1 ; j < array.length ; j++){
+//         console.log(array[i] , "+" , array[j] , "=" , array[i] + array[j])
+//         count++
+//     }
+// }
+// console.log(count)
+// var array = [0,1,2,3,4,5]
+// var target = 5
+// var sum =0
+// for(i = 0 ; i < array.length ; i++){
+//     for(j = i + 1 ; j < array.length ; j++){
+//         sum = array[i] + array[j];
+//         if(sum == target){
+//             console.log(array[i] , "+" , array[j] , "=" , array[i] + array[j])
+//         }
+        
+//     }
+// }
+var array = [2,5,9,12,18,21]
+for(i = 0 ; i < array.length ; i++){
+    for(j = i + 1 ; j < array.length ; j++){
+        console.log("Sudo power")
+    }
+}
